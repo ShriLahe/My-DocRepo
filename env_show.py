8ce951665e6c249fd1cb816261ad16da9fe44d91
Shree Master Repo <103722689+ShriLahe@users.noreply.github.com>
2023-04-04 17:09:44 +0530

    2 file more

---
+++ env_show.py
@@ -0,0 +1,11 @@
+env="prod"
+list_of_env=["dev", "qa", "prod"]
+print(type(list_of_env))
+
+print("I am in", list_of_env[1])
+
+list_of_env.append("uat")  #inseart at the end of dict
+
+#dir what all methods are available
+
+print(list_of_env.count("dev"))
---
+++ variables.py
@@ -0,0 +1,12 @@
+num_1=100.5
+num_2=200
+is_number=True
+final_output="Result:"
+
+print("Addition is:", end="\n")
+print(num_1+num_2)
+print(type(num_1))
+print(type(num_2))
+print(type(is_number))
+print(type(final_output))
+
