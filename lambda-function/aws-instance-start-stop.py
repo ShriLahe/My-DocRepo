d0bf25b9ff06132bb0b90d054e9a48d3931c1d32
Shree Master Repo <103722689+ShriLahe@users.noreply.github.com>
2023-06-13 16:53:43 +0530

    Update aws-instance-start-stop.py
    if we ant to start then we have to change stop to start

--- lambda-function/aws-instance-start-stop.py
+++ lambda-function/aws-instance-start-stop.py
@@ -1,1 +1,19 @@
-import boto 3
+import boto3
+import json
+
+
+def lambda_handler(event, context):
+    clint = boto.clint("ec2")
+
+    isinstance = []
+
+    myec2 = clint.describe_instances()
+    for printins in myec2["Reservatons"]:
+        for printout in printins["Instances"]:
+            print(printout["InstanceID"], printout["InstanceType"], printout["LaunchTime"], printout["State"]["Name"])
+
+            instance.append(printout[InstanceID])
+
+    clint.stop_instances(InstanceIds=instances)
+    print("Instances are stopped")
+    
