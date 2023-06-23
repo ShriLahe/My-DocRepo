3118e2879866ea8da40b9e480e25f42c682ba4fc
Shree Master Repo <103722689+ShriLahe@users.noreply.github.com>
2023-06-23 11:49:50 +0530

    Update list-dynamodb-table.py
    list all table

--- lambda-function/list-dynamodb-table.py
+++ lambda-function/list-dynamodb-table.py
@@ -1,3 +1,4 @@
+#that checks the existence of a DynamoDB table:
 import boto3
 
 def lambda_handler(event, context):
@@ -13,3 +14,19 @@ def lambda_handler(event, context):
         print(f"Table details: {response}")
     except dynamodb.exceptions.ResourceNotFoundException:
         print(f"DynamoDB table '{table_name}' does not exist!")
+#=======================================================================
+# that lists all DynamoDB tables in the AWS account:
+import boto3
+
+def lambda_handler(event, context):
+    dynamodb = boto3.client('dynamodb')
+
+    response = dynamodb.list_tables()
+
+    if 'TableNames' in response:
+        table_names = response['TableNames']
+        print(f"Existing DynamoDB tables:")
+        for table_name in table_names:
+            print(table_name)
+    else:
+        print("No DynamoDB tables found.")
